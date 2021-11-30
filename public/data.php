4257375f58aed653ad23fe9c0637e2325628d289
Irfan Mansha <irfanmansha@gmail.com>
2021-11-30 18:54:22 +0500

    Added Hassan data

--- public/data.php
+++ public/data.php
@@ -1,8 +1,15 @@
 <?php
 
+$name = "Ali";
+$email = "ali@gmail.com";
+$mobile = "0321-9211762";
+$address = "Lahore";
+
 $name = "Irfan";
-$email = "irfanmansha@gmail.com"
-$mobile = "0300-9211762"
-$address = "Multan"
+$email = "irfanmansha@gmail.com";
+$mobile = "0300-9211762";
+$address = "Multan";
+
+
 
 ?>
